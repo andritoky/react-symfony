492acc2df5be9aae3157f47f120996ef7e3ee851
andritoky <randriamampionona9@gmail.com>
2022-05-27 16:36:35 +0200

    authentification ok

--- src/component.tsx/navigation/login.tsx
+++ src/component.tsx/navigation/login.tsx
@@ -7,13 +7,14 @@ interface DataResponse {
     status: number,
     authentification: boolean,
     message: string,
-    class: string
+    class: string,
+    error: string
 }
 
 function Login() {
 
     let navigate = useNavigate()
-    let name = useRef<HTMLInputElement | null>(null),
+    let email = useRef<HTMLInputElement | null>(null),
         password = useRef<HTMLInputElement | null>(null)
 
     let [res, setResponse] = useState<DataResponse>()
@@ -33,7 +34,7 @@ function Login() {
     }
     let login = async () => {
         let login_data = {
-            name: name.current?.value,
+            email: email.current?.value,
             password: password.current?.value,
         }
         let myHeaders = new Headers({
@@ -45,7 +46,7 @@ function Login() {
             headers: myHeaders,
             body: JSON.stringify(login_data)
         }
-        let reponse = await fetch('http://localhost:8000/auth/login', requestOption)
+        let reponse = await fetch('http://localhost:8000/login', requestOption)
         let reponse_status = await reponse.json()
         console.log(reponse_status);
         setResponse(reponse_status)
@@ -64,8 +65,8 @@ function Login() {
         <div className="block-login">
             <div className="box-login">
                 <h3>Login </h3>
-                <label >Name   </label>
-                <input ref={name} type="text" placeholder=" name" className="form-control" />
+                <label >Email   </label>
+                <input ref={email} type="text" placeholder=" email" className="form-control" />
                 <label >Password  </label>
                 <input ref={password} type="password" placeholder=" password" className="form-control" />
 
@@ -76,6 +77,7 @@ function Login() {
                 </div>
                 <br />
                 {message ? <div className={res?.class}>{message}</div> : ""}
+                {res?.error ? <div className="alert alert-danger">{res?.error}</div> : ""}
 
                 <br />
 
